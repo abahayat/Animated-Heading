4162a2a79430f7ed0f729eee6ce74e9fe6179c36
abahayat <abahayat48@gmail.com>
2021-03-08 17:13:31 +0600

    upload files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    
+    $(function() {
+        $('.hero-text-box').animatedHeadline({
+            animationType: 'clip'
+        });
+    })
+    
+    
+});
